db5094c90c6c78a6539a0355e3ef3e805c0ea9aa
01tilinfinity <kuwinter0209@gmail.com>
2025-08-21 14:46:20 +0900

    feat:String[] 형변환

--- src/pages/RoomDetail/RoomDetailPage.tsx
+++ src/pages/RoomDetail/RoomDetailPage.tsx
@@ -1,6 +1,7 @@
 import { useParams, useNavigate } from "react-router-dom";
 import React, { useMemo, useState } from "react";
 import MOCK from "@/mock/listings";
+import ImageMosaic from "@/components/ui/image-mosaic";
 
 export default function RoomDetailPage() {
   const { roomId } = useParams();
